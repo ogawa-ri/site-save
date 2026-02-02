68c8957af9ce5412701c3eedd22f5dd04c5cc85b
ogawa-ri <s7023326@s.akita-u.ac.jp>
2026-02-02 11:06:49 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "site-save-v1";
+const CACHE_NAME = "site-save-v2"; // ★ バージョン変更重要
 const urlsToCache = [
   "./",
   "./index.html",
@@ -6,7 +6,9 @@ const urlsToCache = [
   "./script.js"
 ];
 
+// インストール時
 self.addEventListener("install", event => {
+  self.skipWaiting(); // ★ 即有効化
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
@@ -14,6 +16,23 @@ self.addEventListener("install", event => {
   );
 });
 
+// 有効化時
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // ★ 古いキャッシュ削除
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); // ★ 即反映
+});
+
+// 通信時
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
